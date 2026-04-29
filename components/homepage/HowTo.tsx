dfb208c214037b6fb9ef293e9e24b3973d83a2eb
Gabriel Zanghelini <zanghelinigab@gmail.com>
2026-04-29 15:29:13 -0300

    adding how to content

--- components/homepage/HowTo.tsx
+++ components/homepage/HowTo.tsx
@@ -1,9 +1,11 @@
+import HowToSteps from './HowToSteps'
 import Title from './Title'
 
 const HowTo = () => {
   return (
     <section className='mt-16'>
       <Title title='Como funciona?' />
+      <HowToSteps />
     </section>
   )
 }
